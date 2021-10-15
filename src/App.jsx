1bf57a3308c886140f12c754b661a9ddd37ea19b
brvarner <bvarner4592@gmail.com>
2021-10-15 12:00:37 -0500

    added more functionality and data

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
 
-    const myList = [{
-      id: 1,
-      msg: 'Howdy!'
-    },
-    {
-      id: 2,
-      msg: 'nuttin much lol'
-    },
-    {
-      id: 3,
-      msg: 'bet'
-    },
-  ];
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const allUsers = await res.json();
+      setUsers(allUsers);
+    })();
+  }, []);
 
-    return (
-      <>
-
-        <ul>
-          {myList.map(val => <li key={`my-fancy-item-${val.id}`}>{val.msg}</li>)}
-        </ul>
-
-      </>
-    );
+  return (
+    <main className="container">
+      <section className="row justify-content-center mt-5">
+        {users.map((user) => (
+          <div className="col-md-6" key={`user-card-${user.id}`}>
+            <div className="card shadow my-2">
+              <div className="card-body">
+                <h4 className="card-title">{user.username}</h4>
+                <p className="card-subtitle text-muted">{user.name}</p>
+                <p className="card-text">{user.email} / {user.company.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default App;
